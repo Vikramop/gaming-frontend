5803935f2397a6bc77f51751f8cb3c01322b47b0
Veervikram singh <veervikramsingh132@gmail.com>
2023-07-25 16:19:27 +0530

    added contract address

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,83 @@
 // import Clipboard from '../assets/clipboard.svg';
+import { toast } from 'react-toastify';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { Typography, IconButton } from '@mui/material';
 
 const Home = () => {
   return (
     <div className="home">
-      <div className=" flex justify-center">
-        <div>
-          <p className="text-xl font-semibold">Contract addresss</p>
-          <p className="text-xl font-semibold">
-            0x994A258c7Dec633b5b15376f850D5Ea701179E79
-          </p>
-        </div>
-        <div>
-          {/* <button className="clip">
-            <img src={Clipboard} />
-          </button> */}
+      <div className=" flex justify-center ">
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            marginTop: '34px',
+
+            background: '#1A1A1A',
+            padding: '12px',
+            borderRadius: '8px',
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: 'white',
+              width: 6,
+              marginRight: 10,
+              borderRadius: '10px',
+            }}
+          />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'start',
+              alignItems: 'start',
+            }}
+          >
+            <Typography variant="body" fontWeight="bold" color="white">
+              Contract addresss
+            </Typography>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '4px',
+                alignItems: 'center',
+              }}
+            >
+              <Typography
+                variant="body"
+                color="grey"
+                nowrap
+                sx={{
+                  maxWidth: { xs: '270px', sm: 'fit-content' },
+                  wordWrap: 'break-word',
+                }}
+              >
+                0x994A258c7Dec633b5b15376f850D5Ea701179E79
+              </Typography>
+
+              <IconButton
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    '0x994A258c7Dec633b5b15376f850D5Ea701179E79'
+                  );
+                  toast('Address copied', {
+                    position: 'top-center',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'dark',
+                  });
+                }}
+              >
+                <ContentCopyIcon sx={{ color: 'white', width: '20px' }} />
+              </IconButton>
+            </div>
+          </div>
         </div>
       </div>
       <div className="py-12 herotext">
