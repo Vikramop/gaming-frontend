9e4c6bda0d7885c12448b42e38e9e34566670758
Veervikram singh <veervikramsingh132@gmail.com>
2023-07-27 15:08:39 +0530

    sm changes

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Img from '../assets/game2.jpeg';
 import Img2 from '../assets/gamepvp.jpeg';
-import rightIcon from '../assets/right.png';
+import Mario from '../assets/mario.jpg';
+import Football from '../assets/football.jpg';
 
 const Games = () => {
   return (
@@ -10,6 +11,7 @@ const Games = () => {
         <h1> Coming Soon...</h1>
         <p>Coming soon with a new game daily...</p>
       </div>
+
       <div className="img-sec gap-1 p-2">
         <div className="img-sec2">
           <img src={Img2} alt={Img} />
@@ -29,6 +31,33 @@ const Games = () => {
             <a>
               <p>
                 Live on 27/07/2023 <span> {'>'}</span>
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="img-sec gap-1 p-2">
+        <div className="img-sec2">
+          <img src={Mario} alt={Img} />
+          <div className="img-s2h">
+            <h5>Mario Kart</h5>
+            <a>
+              <p>
+                Live on 07/08/2023 <span>{'>'}</span>
+              </p>
+            </a>
+          </div>
+        </div>
+        <div className="img-sec1 text-center">
+          <img src={Football} alt={Img} />
+          <div className="img-s1h">
+            <h5>
+              Football Premier <br />
+              League
+            </h5>
+            <a>
+              <p>
+                Live on 11/08/2023 <span> {'>'}</span>
               </p>
             </a>
           </div>
